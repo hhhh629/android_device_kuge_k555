376464185e092b67299de93962ae3cf31da0e8eb
hhhh629 <3553230554@qq.com>
2025-09-13 15:23:30 +0800

    Update omni_k555.mk

--- omni_k555.mk
+++ omni_k555.mk
@@ -8,8 +8,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from k555 device
 $(call inherit-product, device/kuge/k555/device.mk)
